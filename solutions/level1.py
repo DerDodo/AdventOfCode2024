1628108e42cb178d12671af5938a7159094902ac
Dominik "Dodo" Dopplinger <dom.dopplinger@gmail.com>
2024-12-04 00:34:11 +0100

    Cleanup level 1

--- solutions/level1.py
+++ solutions/level1.py
@@ -1,44 +1,6 @@
 from collections import defaultdict
 
-from util.file_util import read_input_file_id, read_input_file
-
-
-def process_line(line: str) -> int:
-    digits = list(filter(str.isdigit, line))
-    return int(digits[0]) * 10 + int(digits[-1])
-
-
-def process_line_with_text(line: str) -> int:
-    digits = []
-    for idx, character in enumerate(line):
-        if character.isdigit():
-            digits.append(int(character))
-        else:
-            text_number = is_text_number(line, idx)
-            if text_number:
-                digits.append(text_number)
-
-    return digits[0] * 10 + digits[-1]
-
-
-def is_text_number(line: str, idx: int) -> int | None:
-    search_texts = [
-        ["one", 1],
-        ["two", 2],
-        ["three", 3],
-        ["four", 4],
-        ["five", 5],
-        ["six", 6],
-        ["seven", 7],
-        ["eight", 8],
-        ["nine", 9],
-    ]
-
-    for search_text in search_texts:
-        if line[idx:idx + len(search_text[0])] == search_text[0]:
-            return search_text[1]
-
-    return None
+from util.file_util import read_input_file
 
 
 def parse_input_file() -> tuple[list[int], list[int], dict[int, int]]:
