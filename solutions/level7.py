b456c83bc04272a5b436310d1280bd3b0ce43202
Dominik "Dodo" Dopplinger <dom.dopplinger@gmail.com>
2024-12-08 02:01:50 +0100

    Shortened level 7

--- solutions/level7.py
+++ solutions/level7.py
@@ -1,5 +1,5 @@
 import math
-from enum import Enum
+from operator import mul, add
 
 from util.file_util import read_input_file
 
@@ -8,10 +8,8 @@ def count_digits(n):
     return math.floor(math.log10(abs(n))) + 1
 
 
-class Operation(Enum):
-    Add = 0
-    Multiply = 1
-    Concatenate = 2
+def concatenate(a: int, b: int) -> int:
+    return a * int(math.pow(10, count_digits(b))) + b
 
 
 class Equation:
@@ -23,27 +21,19 @@ class Equation:
         self.result = int(split[0])
         self.numbers = list(map(int, split[1].split(" ")))
 
-    def can_evaluate(self, operations: list[Operation]) -> bool:
+    def can_evaluate(self, operations: list) -> bool:
         return self._can_evaluate(operations, self.numbers[0])
 
-    def _can_evaluate(self, operations: list[Operation], result: int, i: int = 1) -> bool:
+    def _can_evaluate(self, operations: list, result: int, i: int = 1) -> bool:
         if result > self.result:
             return False
         if i == len(self.numbers):
             return result == self.result
+
         for operation in operations:
-            if operation == Operation.Add:
-                new_result = result + self.numbers[i]
-                if self._can_evaluate(operations, new_result, i + 1):
-                    return True
-            elif operation == Operation.Multiply:
-                new_result = result * self.numbers[i]
-                if self._can_evaluate(operations, new_result, i + 1):
-                    return True
-            elif operation == Operation.Concatenate:
-                new_result = result * int(math.pow(10, count_digits(self.numbers[i]))) + self.numbers[i]
-                if self._can_evaluate(operations, new_result, i + 1):
-                    return True
+            new_result = operation(result, self.numbers[i])
+            if self._can_evaluate(operations, new_result, i + 1):
+                return True
 
 
 def parse_input_file() -> list[Equation]:
@@ -53,13 +43,8 @@ def parse_input_file() -> list[Equation]:
 def level7() -> tuple[int, int]:
     equations = parse_input_file()
 
-    total_calibration_result_1 = 0
-    total_calibration_result_2 = 0
-    for equation in equations:
-        if equation.can_evaluate([Operation.Add, Operation.Multiply]):
-            total_calibration_result_1 += equation.result
-        if equation.can_evaluate([Operation.Add, Operation.Multiply, Operation.Concatenate]):
-            total_calibration_result_2 += equation.result
+    total_calibration_result_1 = sum(map(lambda e: e.result if e.can_evaluate([add, mul]) else 0, equations))
+    total_calibration_result_2 = sum(map(lambda e: e.result if e.can_evaluate([add, mul, concatenate]) else 0, equations))
 
     return total_calibration_result_1, total_calibration_result_2
 
