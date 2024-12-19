bacc510862ea29f4c13ceb188f9c6ba3efc8f600
Dominik "Dodo" Dopplinger <dominik.dopplinger@cloudflight.io>
2024-12-19 13:20:43 +0100

    Cleanup code

--- solutions/level19.py
+++ solutions/level19.py
@@ -1,13 +1,17 @@
 from util.file_util import read_input_file
 from util.run_util import RunTimer
 
+
 def parse_input_file() -> tuple[list[str], list[str]]:
     lines = read_input_file(19)
     towels = lines[0].split(", ")
     return towels, lines[2:]
 
 
-def calc_num_patterns(pattern: str, i: int, towels: list[str], result_cache: dict[int, int]) -> int:
+def calc_num_patterns(pattern: str, towels: list[str], i: int = 0, result_cache: dict[int, int] | None = None) -> int:
+    if result_cache is None:
+        result_cache = dict()
+
     if i in result_cache:
         return result_cache[i]
 
@@ -18,20 +22,17 @@ def calc_num_patterns(pattern: str, i: int, towels: list[str], result_cache: dic
             if new_i == len(pattern):
                 num_combinations += 1
             else:
-                num_combinations += calc_num_patterns(pattern, new_i, towels, result_cache)
+                num_combinations += calc_num_patterns(pattern, towels, new_i, result_cache)
+
     result_cache[i] = num_combinations
     return num_combinations
 
 
 def level19() -> tuple[int, int]:
     towels, patterns = parse_input_file()
-    num_possible_patterns = 0
-    num_total_combinations = 0
-    for pattern in patterns:
-        num_combinations = calc_num_patterns(pattern, 0, towels, dict())
-        num_total_combinations += num_combinations
-        if num_combinations > 0:
-            num_possible_patterns += 1
+    num_combinations = list(map(lambda p: calc_num_patterns(p, towels), patterns))
+    num_possible_patterns = sum(c > 0 for c in num_combinations)
+    num_total_combinations = sum(num_combinations)
     return num_possible_patterns, num_total_combinations
 
 
