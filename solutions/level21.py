54a2b5f71947bae2a56091323cd1af292f1c9bbf
Dominik "Dodo" Dopplinger <dom.dopplinger@gmail.com>
2024-12-23 00:18:19 +0100

    Solution day 21

--- solutions/level21.py
+++ solutions/level21.py
@@ -9,7 +9,26 @@ def parse_input_file() -> list[str]:
     return read_input_file(21)
 
 
-Option = list[Direction | str]
+class Option:
+    commands: list[Direction | str]
+    hash: int
+
+    def __init__(self, commands: list[Direction | str]):
+        self.commands = commands
+        self.hash = 0
+        for i in reversed(range(len(commands) - 1)):
+            self.hash = self.hash * 9 + commands[i].hash_value
+
+
+class OptionList:
+    options: list[Option]
+    hash: int
+
+    def __init__(self, options: list[Option]):
+        self.options = options
+        self.hash = 0
+        for i in reversed(range(len(options))):
+            self.hash = self.hash * 100000 + options[i].hash
 
 
 ILLEGAL = "!"
@@ -57,7 +76,7 @@ def get_buttons(start: Position, end: Position) -> list[tuple[Direction, int]]:
 A_B = [(0, 1), (1, 0)]
 
 
-def create_options_with_a(start: Position, new_movements: list[tuple[Direction, int]], forbidden_spot: Position) -> list[Option]:
+def create_options_with_a(start: Position, new_movements: list[tuple[Direction, int]], forbidden_spot: Position) -> OptionList:
     if len(new_movements) == 2:
         options = []
 
@@ -65,20 +84,20 @@ def create_options_with_a(start: Position, new_movements: list[tuple[Direction, 
             if (start.x + new_movements[a][0].x * new_movements[a][1] != forbidden_spot.x or
                     start.y + new_movements[a][0].y * new_movements[a][1] != forbidden_spot.y):
                 movements = [new_movements[a][0]] * new_movements[a][1] + [new_movements[b][0]] * new_movements[b][1] + ["A"]
-                options.append(movements)
+                options.append(Option(movements))
 
-        return options
+        return OptionList(options)
     elif len(new_movements) == 1:
         movements = [new_movements[0][0]] * new_movements[0][1] + ["A"]
-        return [movements]
+        return OptionList([Option(movements)])
     elif len(new_movements) == 0:
-        return [["A"]]
+        return OptionList([Option(["A"])])
     else:
         raise ValueError(f"Invalid movements {new_movements}")
 
 
 class Cache:
-    values: dict[Direction | str, dict[Direction | str, list[Option]]]
+    values: dict[Direction | str, dict[Direction | str, OptionList]]
 
     def __init__(self, pad: dict):
         self.values = defaultdict(dict)
@@ -96,24 +115,31 @@ directional_cache = Cache(directional_pad)
 
 
 def solve_code(code: str, num_robots: int) -> int:
-    parts = create_options(list(code), keypad, keypad_cache)
+    parts = create_options_keypad(list(code), keypad, keypad_cache)
     min_len = sum(map(lambda part: solve_codes_directional(part, num_robots), parts))
     return calc_complexity(code, min_len)
 
 
-def solve_codes_directional(codes: list[Option], num_robots: int) -> int:
+solution_cache: dict[int, dict[int, int]] = defaultdict(dict)
+
+
+def solve_codes_directional(codes: OptionList, num_robots: int) -> int:
     if num_robots == 0:
-        return min(map(len, codes))
+        return min(map(lambda code: len(code.commands), codes.options))
+
+    if codes.hash in solution_cache[num_robots]:
+        return solution_cache[num_robots][codes.hash]
 
     min_len = 9999999999999999999
-    for code in codes:
-        parts = create_options(code, directional_pad, directional_cache)
+    for code in codes.options:
+        parts = create_options_directional(code, directional_pad, directional_cache)
         min_parts_len = sum(map(lambda part: solve_codes_directional(part, num_robots - 1), parts))
         min_len = min(min_len, min_parts_len)
+    solution_cache[num_robots][codes.hash] = min_len
     return min_len
 
 
-def create_options(code: Option, pad: dict, cache: Cache) -> list[list[Option]]:
+def create_options_keypad(code: list[str], pad: dict, cache: Cache) -> list[OptionList]:
     parts = []
     position = pad["A"]
     for button in code:
@@ -123,14 +149,29 @@ def create_options(code: Option, pad: dict, cache: Cache) -> list[list[Option]]:
     return parts
 
 
+option_cache = dict[int, list[OptionList]]()
+
+
+def create_options_directional(code: Option, pad: dict, cache: Cache) -> list[OptionList]:
+    if code.hash in option_cache:
+        return option_cache[code.hash]
+
+    parts = []
+    position = pad["A"]
+    for button in code.commands:
+        target = pad[button]
+        parts.append(cache.values[position][target])
+        position = target
+    option_cache[code.hash] = parts
+    return parts
+
+
 def calc_complexity(code: str, min_len: int) -> int:
-    print(f"{min_len} * {int(code.replace('A', ''))} = {min_len * int(code.replace('A', ''))}")
     return min_len * int(code.replace("A", ""))
 
 
 def calc_complexity_old(code: str, patterns: list[Option]) -> int:
     pattern_len = min(map(lambda p: len(p.movements), patterns))
-    print(f"{pattern_len} * {int(code.replace('A', ''))} = {pattern_len * int(code.replace('A', ''))}")
     return pattern_len * int(code.replace("A", ""))
 
 
@@ -148,4 +189,5 @@ if __name__ == '__main__':
 
 def test_level21():
     assert level21(2) == 126384
-    level21(11)
+    # runtime check
+    level21(25)
